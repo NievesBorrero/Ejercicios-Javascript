c2a004863f12c5eea60db0a998bfcf60564aa018
Nieves <you@example.com>
2017-12-06 14:05:51 +0100

    Añadidas comprobaciones

--- formulario con objetos/js/tester.js
+++ formulario con objetos/js/tester.js
@@ -1,17 +1,22 @@
 /**
-* Pseudoclase 
+* @author Nieves Borrero Barea
+* @version 1.0
+*/
+
+/**
+* Objeto literal para comprobar los inputs del formulario
 */
 
 let tester ={
-
+	// closure
 	patrones: {
 		nombre: [/[A-Za-z]+/, 'No puede estar vacio'],
 		dni: [/^(\d{8})[-\s]?([A-Za-z])$/, 'Ocho dígitos y su letra correspondiente'],
-		fecha: [/^(0?[1-9]|[12][0-9]|3[01])[-/](0?[1-9]|1[012])[-/](\\d{1,4})$/, 'Formatos válidos: 02/02/02 02-02-02 02-02-2002'],
-	    email: [/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/, 'Formato válido: correo@gamil.com'],
-		telf: [/^[6789]\\d{8}$/, "Nueve dígitos, empezando por 6, 7, 8 o 9"],
-	    url: [/^(http:\/\/)?www\\.(\\w)*\\.(\\w){2,3}/, 'Formatos válidos: www.web.es http://www.web.com'],
-	    cuenta: [/^(\\d{4}[-\\s]){2}\\d{2}[-\\s]\\d{10}$/, 'Formato válido: xxxx xxxx xx xxxxxxxxxx (y con - en los espacios)']
+		fecha: [/^(0?[1-9]|[12][0-9]|3[01])[-/](0?[1-9]|1[012])[-/](\d{1,4})$/, 'Formatos válidos: 02/02/02 02-02-02 02-02-2002'],
+	    email: [/^[\w-\.]+@([\w]{2,}\.)+([\w-]{2,})$/, 'Formato válido: correo@gamil.com'],
+		telf: [/^[6789]\d{8}$/, "Nueve dígitos, empezando por 6, 7, 8 o 9"],
+	    url: [/^(http:\/\/)?www\.(\w)*\.(\w){2,3}/, 'Formatos válidos: www.web.es http://www.web.com'],
+	    cuenta: [/^(\d{4}[-\s]){2}\d{2}[-\s]\d{10}$/, 'Formato válido: xxxx xxxx xx xxxxxxxxxx (y con - en los espacios)']
 	},
 
 	testNombre: function (texto){
@@ -51,10 +56,10 @@ let tester ={
 	},
 
 
-	test: function (patron, texto){
-		if(patron[0].test(texto))
-			return '';
-		return patron[1]; // Si no coincide, devuelve la cadena con el error
+	testFecha: function (texto){
+		if(!this.patrones.fecha[0].test(texto))
+			return this.patrones.fecha[1];
+		return this.testNewFecha(texto); // Si no coincide, devuelve la cadena con el error
 		
 	},
 
@@ -65,9 +70,7 @@ let tester ={
 			return this.testLetraDni(texto);
 	},
 
-	testLetraDni: function (dni){
-
-		// De lo contrario...		
+	testLetraDni: function (dni){		
 		let match = this.patrones.dni[0].exec(dni);
 		let numero = match[1]; // Cojo la parte que corresponde al (ámbito) 1 de la expresión regular
 		let letraDni = match[2]; // Cojo el ámbito 2
@@ -81,16 +84,13 @@ let tester ={
 		return '';
 	},
 
-	testFecha: function (fecha){
-		let msj = this.test(this.patrones.fecha, fecha);
-		if(msj.length!==0) // Si hay mensaje de error, lo devuelve
-			return msj;
-		// De lo contrario...
+
+	testNewFecha: function (fecha){
 		fecha = fecha.replace(/[- ]/g, '/');
 		let enTrocitos = fecha.split('/');
 		let nuevaFecha = new Date (enTrocitos[2], enTrocitos[1], enTrocitos[0]);
 
-		if(nuevaFecha.getMonth()!=enTrocitos[1]-1) // PREGUNTAR
+		if(nuevaFecha.getMonth()!=enTrocitos[1]/*-1*/) // PREGUNTAR
 			return 'Fecha no existe';
 		return '';
 	}
